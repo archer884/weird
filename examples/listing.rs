7e91f0fa3fb7a6b903227c941c29910ae86f2040
J/A <archer884@gmail.com>
2018-01-23 18:14:06 -0600

    Use xor shift

--- examples/listing.rs
+++ examples/listing.rs
@@ -5,13 +5,9 @@ use weird::*;
 fn main() {
     let salt = b"In the beginning, God created the heavens and the earth. \
         And the earth was without form, and void; and darkness was upon the face of the waters.";
-
-    let weird = Weird::new(
-        CrockfordAlphabet,
-        Salt { data: salt },
-    );
+    let weird = Weird::with_salt(salt);
     
-    for n in 1000..2000 {
+    for n in 1..11 {
         let encoded = weird.encode(n);
         let decoded = weird.decode(&encoded).unwrap();
         // assert_eq!(n, decoded);
--- src/lib.rs
+++ src/lib.rs
@@ -6,99 +6,55 @@ pub use error::Error;
 
 static UPPERCASE_ENCODING: &[u8] = b"0123456789ABCDEFGHJKMNPQRSTVWXYZ";
 
-pub struct Weird<A = CrockfordAlphabet, S = Salt> {
-    alphabet: A,
-    salt: S,
-}
+pub struct Weird<T = StaticSalt> { pub salt: T }
 
-pub struct CrockfordAlphabet;
-
-pub trait AlphabetProvider {
-    fn encode<T: SaltSource>(&self, u: u8, s: &mut T) -> u8;
-    fn decode<T: SaltSource>(&self, u: u8, idx: usize, s: &mut T) -> Result<u8>;
-}
-
-impl AlphabetProvider for CrockfordAlphabet {
-    fn encode<T: SaltSource>(&self, u: u8, s: &mut T) -> u8 {
-        let idx = (u as i32 + s.next()) % 32;
-        unsafe {
-            *UPPERCASE_ENCODING.get_unchecked(idx as usize)
-        }
+impl Weird {
+    pub fn with_salt(salt: &'static [u8]) -> Self {
+        Weird { salt: StaticSalt(salt) }
     }
+}
 
-    fn decode<T: SaltSource>(&self, u: u8, idx: usize, s: &mut T) -> Result<u8> {
-        static VALUE_MAPPING: [i8; 256] = include!("../resources/u8-mapping.txt");
+pub struct StaticSalt(&'static [u8]);
 
-        unsafe {
-            match *VALUE_MAPPING.get_unchecked(u as usize) {
-                -1 => Err(Error::new(Kind::InvalidDigit(idx, u), "Invalid encoded digit.")),
-                -2 => Err(Error::new(Kind::CheckDigitUnsupported(idx, u), "Check digits currently unsupported.")),
-                result => {
-                    let offset = (s.next() % 32) as i8;
-                    let result = if result > offset {
-                        result - offset
-                    } else {
-                        offset - result
-                    };
+pub trait SaltProvider {
+    type Salt: Salt;
+    fn get(&self) -> Self::Salt;
+}
 
-                    Ok(result as u8)
-                }
-            }
+impl SaltProvider for StaticSalt {
+    type Salt = StaticSource;
+
+    fn get(&self) -> Self::Salt {
+        StaticSource {
+            idx: 0,
+            data: self.0,
         }
     }
 }
 
-pub struct Salt { pub data: &'static [u8] }
-
-pub struct SaltInstance {
+pub struct StaticSource {
     idx: usize,
     data: &'static [u8],
 }
 
-pub trait SaltProvider {
-    type SaltSource: SaltSource;
-    fn get_instance(&self) -> Self::SaltSource;
+pub trait Salt {
+    fn next(&mut self) -> u8;
 }
 
-impl SaltProvider for Salt {
-    type SaltSource = SaltInstance;
-
-    fn get_instance(&self) -> SaltInstance {
-        SaltInstance { idx: 0, data: self.data }
-    }
-}
-
-pub trait SaltSource {
-    fn next(&mut self) -> i32;
-}
-
-impl SaltSource for SaltInstance {
-    fn next(&mut self) -> i32 {
-        let result = self.data[self.idx];
-        self.idx += 1;
+impl Salt for StaticSource {
+    fn next(&mut self) -> u8 {
         if self.idx >= self.data.len() {
-            self.idx = 0;
+            self.idx = 1;
+            self.data[0]
+        } else {
+            let ret = self.data[self.idx];
+            self.idx += 1;
+            ret
         }
-        result as i32
     }
 }
 
-impl<A, S> Weird<A, S>
-where
-    A: AlphabetProvider,
-    S: SaltProvider,
-{
-    pub fn new(alphabet: A, salt: S) -> Self {
-        Self { alphabet, salt }
-    }
-
-    pub fn with_salt(salt: &'static [u8]) -> Weird<CrockfordAlphabet, Salt> {
-        Weird {
-            alphabet: CrockfordAlphabet,
-            salt: Salt { data: salt },
-        }
-    }
-
+impl<T: SaltProvider> Weird<T> {
     pub fn encode(&self, n: u64) -> String {
         let mut buf = String::new();
         self.encode_to(n, &mut buf);
@@ -117,10 +73,11 @@ where
         const FIVE_SHIFT: usize = 59;
         const FIVE_RESET: usize = 5;
 
-        let mut salt_instance = self.salt.get_instance();
+        let mut salt = self.salt.get();
+        let mut xor = |u| UPPERCASE_ENCODING[(u ^ (salt.next() % 32)) as usize] as char;
 
         if n == 0 {
-            target.push(self.alphabet.encode(0, &mut salt_instance) as char);
+            target.push(xor(0));
             return;
         }
 
@@ -140,19 +97,19 @@ where
             i => {
                 n <<= QUAD_RESET;
                 n |= 1;
-                target.push(self.alphabet.encode(i as u8, &mut salt_instance) as char);
+                target.push(xor(i as u8));
             }
         }
 
         // From now until we reach the stop bit, take the five most significant bits and then shift
         // left by five bits.
         while n != STOP_BIT {
-            target.push(self.alphabet.encode((n >> FIVE_SHIFT) as u8, &mut salt_instance) as char);
+            target.push(xor((n >> FIVE_SHIFT) as u8));
             n <<= FIVE_RESET;
         }
     }
 
-    pub fn decode<T: AsRef<str>>(&self, input: T) -> Result<u64> {
+    pub fn decode<S: AsRef<str>>(&self, input: S) -> Result<u64> {
         const BASE: u64 = 0x20;
 
         let input = input.as_ref();
@@ -160,12 +117,14 @@ where
             0 => Err(Error::new(Kind::EmptyString, "Encoded input string is empty.")),
             n if n > 13 => Err(Error::new(Kind::OutOfRange, "Encoded value is too large.")),
             _ => {
-                let mut salt_instance = self.salt.get_instance();
+                let mut salt = self.salt.get();
+                let mut xor = |u| (u ^ (salt.next() % 32));
+
                 let mut place = BASE.pow(input.len() as u32 - 1);
                 let mut n = 0;
 
                 for (idx, u) in input.bytes().enumerate() {
-                    let digit = self.alphabet.decode(u, idx, &mut salt_instance)?;
+                    let digit = to_normal_digit(idx, u, &mut xor)?;
                     n += u64::from(digit).wrapping_mul(place);
                     place /= BASE;
                 }
@@ -176,181 +135,15 @@ where
     }
 }
 
-#[cfg(test)]
-mod encoding {
-    use super::*;
+/// Attempts to convert an ascii digit to a normalized form.
+fn to_normal_digit<F: FnMut(u8) -> u8>(idx: usize, u: u8, xor: &mut F) -> Result<u8> {
+    static VALUE_MAPPING: [i8; 256] = include!("../resources/u8-mapping.txt");
 
-    #[test]
-    fn zero_returns_zero() {
-        let weird = get_weird();
-        assert_eq!("0", &*weird.encode(0));
-    }
-
-    #[test]
-    fn large_value_returns_correct_large_value() {
-        let weird = get_weird();
-        assert_eq!("1ZZZ", &*weird.encode(65535));
-    }
-
-    #[test]
-    fn x5111_is_4zq() {
-        let weird = get_weird();
-        assert_eq!("4ZQ", &*weird.encode(5111));
-    }
-
-    #[test]
-    fn x18446744073709551615_is_fzzzzzzzzzzzz() {
-        let weird = get_weird();
-        assert_eq!("FZZZZZZZZZZZZ", &*weird.encode(18446744073709551615));
-    }
-
-    struct NullSaltProvider;
-
-    impl SaltProvider for NullSaltProvider {
-        type SaltSource = NullSaltInstance;
-
-        fn get_instance(&self) -> Self::SaltSource {
-            NullSaltInstance
-        }
-    }
-
-    struct NullSaltInstance;
-
-    impl SaltSource for NullSaltInstance {
-        fn next(&mut self) -> i32 { 0 }
-    }
-
-    // Weird with nullsaltprovider is 100% compatible with Crockford Base32.
-    fn get_weird() -> Weird<CrockfordAlphabet, NullSaltProvider> {
-        Weird {
-            alphabet: CrockfordAlphabet,
-            salt: NullSaltProvider,
-        }
-    }
-}
-
-#[cfg(test)]
-mod decoding {
-    use super::*;
-
-    #[test]
-    fn zero_length_strings_fail() {
-        let weird = get_weird();
-        let input = "";
-        let expected = Err(Error::new(Kind::EmptyString, "Don't care"));
-        let actual = weird.decode(input);
-
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn long_strings_fail() {
-        let weird = get_weird();
-        let input = "12345678910121";
-        let expected = Err(Error::new(Kind::OutOfRange, "Don't care"));
-        let actual = weird.decode(input);
-
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn invalid_bytes_fail() {
-        let weird = get_weird();
-        let input = "fZZ!2";
-        let expected = Err(Error::new(Kind::InvalidDigit(3, 33), "Don't care"));
-        let actual = weird.decode(input);
-
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn zero_becomes_zero() {
-        let weird = get_weird();
-        let input = "0";
-        let expected = Ok(0);
-        let actual = weird.decode(input);
-
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn large_values_become_large_values() {
-        let weird = get_weird();
-        assert_eq!(Ok(65535), weird.decode("1zzz"));
-        assert_eq!(Ok(65535), weird.decode("1ZZZ"));
-    }
-
-    #[test]
-    fn map_to_0() {
-        let weird = get_weird();
-        assert_eq!(Ok(0), weird.decode("O"));
-        assert_eq!(Ok(0), weird.decode("o"));
-    }
-
-    #[test]
-    fn map_to_1() {
-        let weird = get_weird();
-        assert_eq!(Ok(1), weird.decode("I"));
-        assert_eq!(Ok(1), weird.decode("i"));
-        assert_eq!(Ok(1), weird.decode("L"));
-        assert_eq!(Ok(1), weird.decode("l"));
-    }
-
-    #[test]
-    fn z_equals_31() {
-        let weird = get_weird();
-        assert_eq!(Ok(31), weird.decode("z"));
-        assert_eq!(Ok(31), weird.decode("Z"));
-    }
-
-    #[test]
-    fn q_equals_23() {
-        let weird = get_weird();
-        assert_eq!(Ok(23), weird.decode("q"));
-        assert_eq!(Ok(23), weird.decode("Q"));
-    }
-
-    #[test]
-    fn four_z_q_works() {
-        let weird = get_weird();
-        assert_eq!(Ok(5111), weird.decode("4zq"));
-        assert_eq!(Ok(5111), weird.decode("4ZQ"));
-    }
-
-    #[test]
-    fn max_value_works() {
-        let weird = get_weird();
-        assert_eq!(Ok(18446744073709551615), weird.decode("fzzzzzzzzzzzz"));
-    }
-
-    #[test]
-    fn u_produces_an_error_instead_of_a_crash() {
-        let weird = get_weird();
-        assert!(weird.decode("iVuv").is_err());
-        assert!(weird.decode("iVUv").is_err());
-    }
-
-    struct NullSaltProvider;
-
-    impl SaltProvider for NullSaltProvider {
-        type SaltSource = NullSaltInstance;
-
-        fn get_instance(&self) -> Self::SaltSource {
-            NullSaltInstance
-        }
-    }
-
-    struct NullSaltInstance;
-
-    impl SaltSource for NullSaltInstance {
-        fn next(&mut self) -> i32 { 0 }
-    }
-
-    // Weird with nullsaltprovider is 100% compatible with Crockford Base32.
-    fn get_weird() -> Weird<CrockfordAlphabet, NullSaltProvider> {
-        Weird {
-            alphabet: CrockfordAlphabet,
-            salt: NullSaltProvider,
+    unsafe {
+        match *VALUE_MAPPING.get_unchecked(xor(u) as usize) {
+            -1 => Err(Error::new(Kind::InvalidDigit(idx, u), "Invalid encoded digit.")),
+            -2 => Err(Error::new(Kind::CheckDigitUnsupported(idx, u), "Check digits not currently supported.")),
+            result => Ok(result as u8),
         }
     }
 }
