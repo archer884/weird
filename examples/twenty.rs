7e2e66859e759ea8c5151fee7e12e6929fd948fa
J/A <archer884@gmail.com>
2021-02-07 13:05:15 -0600

    Derive alphabet from salt or seed

--- examples/twenty.rs
+++ examples/twenty.rs
@@ -1,23 +1,15 @@
-use weird::{Alphabet, Weird, WeirdWithAlphabet};
+use weird::Weird;
 
 // This works now, but I'm not convinced that the randomized alphabet
 // works any better than the salt alone, in the grand scheme of things.
 
 fn main() {
     let salt = "In the beginning God created the heaven and the earth.";
-    let weird = Weird::new(salt);
-    let weirder = WeirdWithAlphabet::new(
-        salt,
-        Alphabet::new("4836Q1XBVM59THGRS7Y2ADW0NFZJKPEC").unwrap(),
-    );
+    let weird = Weird::with_salt(salt);
 
-    for n in 1..=200 {
-        let a = weird.encode(n);
-        let b = weirder.encode(n);
-
-        assert_eq!(weird.decode(&a).unwrap(), n);
-        assert_eq!(weirder.decode(&b).unwrap(), n);
-
-        println!("{:>3}: {:>2} / {:>2}", n, a, b);
+    for identifier in (1..=200).map(|n| n + 1_000_000) {
+        let encoded = weird.encode(identifier);
+        assert_eq!(identifier, weird.decode(&encoded).unwrap());
+        println!("{:>6}: {:>6}", identifier, encoded);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,95 @@
-use std::{collections::HashSet, fmt::{self, Write}};
+use std::{borrow::Borrow, fmt::{self, Write}};
 
 mod error;
-mod salt;
 
 pub use error::*;
-pub use salt::*;
+use rand::prelude::SliceRandom;
+use squirrel::SquirrelRng;
 
-static CANONICAL_MAPPING: [i8; 256] = include!("../resources/u8-mapping.txt");
-static UPPERCASE_ENCODING: &[u8] = b"0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+static CANONICAL_MAPPING: &[i8; 256] = &include!("../resources/u8-mapping.txt");
+static UPPERCASE_ENCODING: &[u8; 32] = b"0123456789ABCDEFGHJKMNPQRSTVWXYZ";
 
+pub trait Salt {
+    fn byte_source(&self) -> ByteSource;
+    fn bytes(&self) -> &[u8];
+}
+
+impl<T: Borrow<str>> Salt for T {
+    fn byte_source(&self) -> ByteSource {
+        ByteSource {
+            idx: 0,
+            data: self.borrow().as_bytes(),
+        }        
+    }
+
+    fn bytes(&self) -> &[u8] {
+        self.borrow().as_bytes()
+    }
+}
+
+pub struct ByteSource<'a> {
+    idx: usize,
+    data: &'a [u8],
+}
+
+impl<'a> ByteSource<'a> {
+    pub(crate) fn next(&mut self) -> u8 {
+        if self.idx == self.data.len() {
+            self.idx = 0;
+            self.next();
+        }
+
+        let ret = self.data[self.idx];
+        self.idx += 1;
+        ret
+    }
+
+    pub(crate) fn apply(&mut self, u: u8) -> u8 {
+        let x = self.next() % 32;
+        u ^ x
+    }
+}
+
+#[derive(Clone)]
 pub struct Alphabet {
     alpha: [u8; 32],
     mapping: [i8; 256],
 }
 
 impl Alphabet {
-    pub fn new(alpha: &str) -> Option<Self> {
-        let mut filter = HashSet::new();
-        let alphabet: Vec<_> = alpha
-            .bytes()
-            // We exclude the same characters as Crockford encoding.
-            .filter(|&x| CANONICAL_MAPPING[x as usize] >= 0 && !b"IiLlOoUu".contains(&x) && filter.insert(x))
-            .map(|u| u.to_ascii_uppercase())
-            .collect();
+    /// Shuffle an `Alphabet` using the provided salt.
+    pub fn from_salt(salt: impl AsRef<[u8]>) -> Self {
+        // FNV hash (probably)
+        const P: u32 = 16777619;
+        const SEED_HASH: u32 = 2166136261;
 
-        if alphabet.len() != 32 {
-            panic!("Invalid alphabet");
-        }
+        let mut hash = salt.as_ref().iter().copied().fold(SEED_HASH, |a, b| (a ^ b as u32).wrapping_mul(P));
+        hash = hash.wrapping_add(hash << 13);
+        hash ^= hash >> 7;
+        hash = hash.wrapping_add(hash << 3);
+        hash ^= hash >> 17;
+        hash = hash.wrapping_add(hash << 5);
 
-        let mut alpha = [0u8; 32];
+        let mut rng = SquirrelRng::with_seed(hash);
+        let mut alphabet = UPPERCASE_ENCODING.clone();
+        alphabet.shuffle(&mut rng);
+
+        Self::from_checked_alphabet(alphabet)
+    }
+
+    /// Shuffle an `Alphabet` using the provided seed.
+    pub fn from_seed(seed: u32) -> Self {
+        let mut rng = SquirrelRng::with_seed(seed);
+        let mut alphabet = UPPERCASE_ENCODING.clone();
+        alphabet.shuffle(&mut rng);
+        Self::from_checked_alphabet(alphabet)
+    }
+
+    // This function is private because calling it with an unchecked alphabet
+    // is wildly unsafe.
+    fn from_checked_alphabet(alpha: [u8; 32]) -> Self {
         let mut mapping = CANONICAL_MAPPING.clone();
 
-        alpha.iter_mut().zip(alphabet).for_each(|(a, b)| *a = b);
-
-        // The canonical character gives us the location of the original value
-        // in the mapping array. The scrambled character provides the location
-        // where that value should be stored. I think.
-        //
-        // Hell, I don't know, I'm tired.
         for (&canonical, &scrambled) in UPPERCASE_ENCODING.iter().zip(alpha.iter()) {
             match scrambled {
                 // When mapping 0, we must also map Oo
@@ -66,20 +118,34 @@ impl Alphabet {
             }
         }
 
-        Some(Self { alpha, mapping })
+        Self { alpha, mapping }
     }
 }
 
-pub struct WeirdWithAlphabet<T> {
-    alphabet: Alphabet,
-    salt: Salt<T>,
+impl fmt::Debug for Alphabet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let alphabet: String = self.alpha.iter().copied().map(|u| u as char).collect();
+        write!(f, "Alphabet({:?})", alphabet)
+    }
 }
 
-impl<T: AsRef<[u8]>> WeirdWithAlphabet<T> {
+pub struct Weird<T> {
+    alphabet: Alphabet,
+    salt: T,
+}
+
+impl<T: Salt> Weird<T> {
+    pub fn with_salt(salt: T) -> Self {
+        Self {
+            alphabet: Alphabet::from_salt(salt.bytes()),
+            salt,
+        }
+    }
+
     pub fn new(salt: T, alphabet: Alphabet) -> Self {
         Self {
             alphabet,
-            salt: Salt(salt),
+            salt,
         }
     }
 
@@ -107,7 +173,7 @@ impl<T: AsRef<[u8]>> WeirdWithAlphabet<T> {
             return Ok(());
         }
 
-        let mut salt = self.salt.get();
+        let mut salt = self.salt.byte_source();
 
         // Start by getting the most significant four bits. We get four here because these would be
         // leftovers when starting from the least significant bits. In either case, tag the four least
@@ -139,7 +205,7 @@ impl<T: AsRef<[u8]>> WeirdWithAlphabet<T> {
         Ok(())
     }
 
-    pub fn decode<S: AsRef<str>>(&self, input: S) -> Result<u64> {
+    pub fn decode(&self, input: impl AsRef<str>) -> Result<u64> {
         fn get_mapping(idx: usize, u: u8, mapping: &[i8]) -> Result<u8> {
             unsafe {
                 match *mapping.get_unchecked(u as usize) {
@@ -166,7 +232,7 @@ impl<T: AsRef<[u8]>> WeirdWithAlphabet<T> {
             )),
             n if n > 13 => Err(Error::new(Kind::OutOfRange, "Encoded value is too large")),
             _ => {
-                let mut salt = self.salt.get();
+                let mut salt = self.salt.byte_source();
                 let mut place = BASE.pow(input.len() as u32 - 1);
                 let mut n = 0;
 
@@ -181,120 +247,6 @@ impl<T: AsRef<[u8]>> WeirdWithAlphabet<T> {
 
                 Ok(n)
             }
-        }
-    }
-}
-
-pub struct Weird<T> {
-    pub salt: Salt<T>,
-}
-
-impl<T: AsRef<[u8]>> Weird<T> {
-    pub fn new(salt: T) -> Self {
-        Weird { salt: Salt(salt) }
-    }
-}
-
-impl<T: AsRef<[u8]>> Weird<T> {
-    pub fn encode(&self, n: u64) -> String {
-        let mut buf = String::with_capacity(13);
-        self.encode_into(n, &mut buf)
-            .expect("Cannot fail to encode into a string");
-        buf
-    }
-
-    pub fn encode_into<W: Write>(&self, mut n: u64, w: &mut W) -> fmt::Result {
-        // After we clear the four most significant bits, the four least significant bits will be
-        // replaced with 0001. We can then know to stop once the four most significant bits are,
-        // likewise, 0001.
-        const STOP_BIT: u64 = 1 << QUAD_SHIFT;
-
-        const QUAD_SHIFT: usize = 60;
-        const QUAD_RESET: usize = 4;
-
-        const FIVE_SHIFT: usize = 59;
-        const FIVE_RESET: usize = 5;
-
-        if n == 0 {
-            w.write_char('0')?;
-            return Ok(());
-        }
-
-        let mut salt = self.salt.get();
-
-        // Start by getting the most significant four bits. We get four here because these would be
-        // leftovers when starting from the least significant bits. In either case, tag the four least
-        // significant bits with our stop bit.
-        match (n >> QUAD_SHIFT) as u8 {
-            // Eat leading zero-bits. This should not be done if the first four bits were non-zero.
-            // Additionally, we *must* do this in increments of five bits.
-            0 => {
-                n <<= QUAD_RESET;
-                n |= 1;
-                n <<= n.leading_zeros() / 5 * 5;
-            }
-
-            // Write value of first four bytes.
-            i => {
-                n <<= QUAD_RESET;
-                n |= 1;
-                w.write_char(UPPERCASE_ENCODING[salt.apply(i) as usize] as char)?;
-            }
-        }
-
-        // From now until we reach the stop bit, take the five most significant bits and then shift
-        // left by five bits.
-        while n != STOP_BIT {
-            w.write_char(UPPERCASE_ENCODING[salt.apply((n >> FIVE_SHIFT) as u8) as usize] as char)?;
-            n <<= FIVE_RESET;
-        }
-
-        Ok(())
-    }
-
-    pub fn decode<S: AsRef<str>>(&self, input: S) -> Result<u64> {
-        const BASE: u64 = 0x20;
-
-        let input = input.as_ref();
-        match input.len() {
-            0 => Err(Error::new(
-                Kind::EmptyString,
-                "Encoded input string is empty.",
-            )),
-            n if n > 13 => Err(Error::new(Kind::OutOfRange, "Encoded value is too large")),
-            _ => {
-                let mut salt = self.salt.get();
-                let mut place = BASE.pow(input.len() as u32 - 1);
-                let mut n = 0;
-
-                for (idx, u) in input.bytes().enumerate() {
-                    let digit = to_normal_digit(idx, u)?;
-                    let digit = salt.apply(digit as u8);
-                    n += u64::from(digit).wrapping_mul(place);
-
-                    // This compiles to >>= 5
-                    place /= BASE;
-                }
-
-                Ok(n)
-            }
-        }
-    }
-}
-
-/// Attempts to convert an ascii digit to a normalized form.
-fn to_normal_digit(idx: usize, u: u8) -> Result<u8> {
-    unsafe {
-        match *CANONICAL_MAPPING.get_unchecked(u as usize) {
-            -1 => Err(Error::new(
-                Kind::InvalidDigit(idx, u),
-                "Invalid encoded digit.",
-            )),
-            -2 => Err(Error::new(
-                Kind::CheckDigitUnsupported(idx, u),
-                "Check digits not currently supported.",
-            )),
-            result => Ok(result as u8),
         }
     }
 }
